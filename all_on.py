05caf25d6b8333cdfb723b017d7b50033526e527
MODMYPI <dave@modmypi.com>
2018-08-31 16:31:43 +0100

    Update all_on.py

--- all_on.py
+++ all_on.py
@@ -1,9 +1,24 @@
+#!/usr/bin/python
+
+#LED1 - 18 - 24
+#LED2 - 16 - 23
+#LED3 - 15 - 22
+#LED4 - 40 - 21
+#LED5 - 38 - 20
+#LED6 - 35 - 19
+#LED7 - 12 - 18
+#LED8 - 11 - 17
+#LED9 - 36 - 16
+#LED10 - 33 - 13
+#LED11 (GREEN) - 31 - 6
+#LED12 (GREEN) - 32 - 12
+
 from RPi import GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-LEDS = (11,16,17,18,19,20,21,22,23,24,12)
+LEDS = (17,22,6,13,19,18,23,24,12,16,20,21)
 
 GPIO.setup(LEDS, GPIO.OUT, initial=1)
 
